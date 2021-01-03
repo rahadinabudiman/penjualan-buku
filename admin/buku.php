f9f7d3a9d3956da56736a62ac3bc932acfb87f72
Raha <erbees.50000@gmail.com>
2021-01-03 14:54:14 +0700

    add tambah buku

--- admin/buku.php
+++ admin/buku.php
@@ -28,7 +28,7 @@
             <td><?php echo $pecah['genre']; ?></td>
             <td><?php echo $pecah['tahun']; ?></td>
             <td><?php echo $pecah['sinopsis_buku'];?></td>
-            <td><?php echo $pecah['foto_buku']; ?></td>
+            <td><img src="foto_buku/<?php echo $pecah['foto_buku']; ?>" width="100"></td>
             <td>
                 <a href="" class="btn btn-danger">HAPUS</a>
                 <a href="" class="btn btn-warning">EDIT</a>
--- admin/tambah_buku.php
+++ admin/tambah_buku.php
@@ -1,6 +1,6 @@
 <h2>Ini tambah buku.</h2>
 
-<form method="POST" enctype="multiform/form-data">
+<form method="POST" enctype="multipart/form-data">
 <div class="form-group">
     <label>Judul Buku</label>
     <input type="text" class="form-control" name="judul">
@@ -18,11 +18,33 @@
     <input type="text" class="form-control" name="genre">
 </div>
 <div class="form-group">
-    <label>Harga Buku</label>
-    <input type="text" class="form-control" name="harga">
+    <label>Sinopsis</label>
+    <input type="text" class="form-control" name="sinopsis">
 </div>
 <div class="form-group">
-    <input class="input--style-2 js-datepicker" type="text" placeholder="Birthdate" name="birthday">
-    <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+    <label>Harga Buku</label>
+    <input type="number" class="form-control" name="harga">
 </div>
+<div class="form-group">
+    <label>Harga Buku</label>
+    <input type="date" class="form-control" name="tahun">
+</div>
+<div class="form-group">
+    <label>Foto Buku</label>
+    <input type="file" class="form-control" name="foto">
+</div>
+<button class="btn btn-primary" name="simpan">Save</button>
 </form>
+<?php
+ if(isset($_POST['simpan'])){
+    $nama = $_FILES['foto']['name'];
+    $lokasi = $_FILES['foto']['tmp_name'];
+    move_uploaded_file($lokasi,"foto_buku/".$nama);
+    $koneksi->query("INSERT INTO buku
+        (judul_buku,penulis_buku,penerbit_buku,genre,foto_buku,harga_buku,sinopsis_buku,tahun) VALUES ('$_POST[judul]','$_POST[penulis]','$_POST[penerbit]','$_POST[genre]','$nama','$_POST[harga]','$_POST[sinopsis]','$_POST[tahun]')");
+    
+    echo "<div class='alert alert-info'>Data Tersimpan</div>";
+    echo "<meta http-equiv='refresh' content='1;url=index.php?halaman=buku'>";
+}
+    
+?>
