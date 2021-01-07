0c466f75bfdfefff58024eb3fa116e5785b2d5ab
Raha <erbees.50000@gmail.com>
2021-01-07 10:02:22 +0700

    repairing database

--- database/db_buku.sql
+++ database/db_buku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Jan 2021 pada 21.21
+-- Waktu pembuatan: 07 Jan 2021 pada 03.59
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.4.13
 
